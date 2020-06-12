b933ce1a8ffc9100c6522cdc28edb89e9a2d80af
kamrankhosa <37815528+kamrankhosa@users.noreply.github.com>
2020-06-12 10:26:05 -0700

    updated app.js and app.css added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 function App() {
+  let [count, acountState] = useState(0);
   return (
-    <div>
-      Test to deploy project
+    <div className="box">
+      <h3>Value of counter is : {count} </h3>
+      <button onClick={
+        ()=>acountState(++count)
+        }>Update counter</button>
     </div>
   );
 }
